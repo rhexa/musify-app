1b5660eb3990aa4be9bf0d0e3bf1423108691aea
CihanErenler <cihanerenler@outlook.com>
2021-04-26 07:36:38 +0300

    filtering feature was implemented to create playlist page

--- js/createPlaylist.js
+++ js/createPlaylist.js
@@ -6,6 +6,9 @@ const uploadimage = document.querySelectorAll(".upload-playlist-pic");
 const coverPic = document.querySelectorAll(".playlist-cover-pic");
 const musicIcon = document.querySelectorAll(".fa-music");
 const symbol = document.querySelector(".playlist-card-symbol");
+const deleteInput = document.querySelector("#delete-input");
+const searchInput = document.querySelector(".search-input");
+const filteredSongs = document.querySelector(".filtered-songs");
 
 // Upload image
 uploadimage.forEach((item) => {
@@ -38,6 +41,69 @@ uploadimage.forEach((item) => {
   });
 });
 
+// Fetch the song
+function fetchSong(x) {
+  fetch("js/data.json")
+    .then((res) => res.json())
+    .then((res) => {
+      // filter the result array according to entered input value
+      const list = res.filter(
+        (i) => i.name.toLowerCase().indexOf(x.toLowerCase()) > -1
+      );
+
+      // if list lenght is 0 it means there is no mathing song
+      //in that case show the no result text
+      if (list.length === 0) {
+        filteredSongs.innerHTML = `<h1 class="no-result">No results found for "${x}"</h1>`;
+      } else {
+        //if list length is greater than 0 then render the songs to dom
+        generateSong(list);
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+function generateSong(list) {
+  const songs = list
+    .map(
+      (i) => `
+      <div class="filtered-song" data-id="${i.id}">
+        <div class="filtered-song-img-wrap">
+          <i class="fas fa-play play-filtered-song"></i>
+          <img src="song-covers/${i.img}" alt="${i.name}" />
+        </div>
+        <div class="filtered-song-info-wrap">
+          <h3 class="filtered-song-name">${i.name}</h3>
+          <p class="filtered-artist-name">${i.artist}</p>
+        </div>
+        <div class="filtered-song-btn">
+          <button  class="btn add-to-playlist">Add</button>
+        </div>
+      </div>
+  `
+    )
+    .join("");
+  filteredSongs.innerHTML = songs;
+
+  const playFiltered = document.querySelectorAll(".filtered-song-img-wrap > i");
+  const appended = filteredSongs.children;
+
+  playFiltered.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      const id = appended[index].getAttribute("data-id");
+
+      fetch("js/data.json")
+        .then((res) => res.json())
+        .then((res) => {
+          const one = res.find((s) => s.id === id);
+
+          setAudioSong(one);
+          playAudioSong();
+        });
+    });
+  });
+}
+
 // Eventlisteners
 window.addEventListener("click", (e) => {
   if (
@@ -62,6 +128,25 @@ window.addEventListener("click", (e) => {
       playlistName.textContent = "My Playlist#2";
     }
   }
+});
+
+searchInput.addEventListener("keyup", (e) => {
+  const word = e.target.value;
+
+  if (word.length > 0) {
+    fetchSong(word);
+    deleteInput.style.display = "flex";
+  } else {
+    deleteInput.style.display = "none";
+    filteredSongs.innerHTML = "";
+  }
+});
+
+deleteInput.addEventListener("click", () => {
+  searchInput.value = "";
+  deleteInput.style.display = "none";
+  searchInput.focus();
+  filteredSongs.innerHTML = "";
 });
 
 editPlaylistName.addEventListener("keyup", (e) => {
